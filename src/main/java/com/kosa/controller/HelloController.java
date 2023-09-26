0a9af083e19a4ebcb58ed0eeaef3c91af51a7e60
dolsam77 <dolsam77@nate.com>
2023-09-26 10:58:12 +0900

    insertl HelloController

---
+++ src/main/java/com/kosa/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.kosa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello Kosa!!!");
+		
+		return "hello";
+	}
+
+}
