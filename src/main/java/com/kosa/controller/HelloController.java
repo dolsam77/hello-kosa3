4e27460635701ddb71622e00ca6c710797dc975d
dolsam77 <dolsam77@nate.com>
2023-09-26 14:04:40 +0900

    update HelloController

--- src/main/java/com/kosa/controller/HelloController.java
+++ src/main/java/com/kosa/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("message", "Hello Kosa2222!!!");
+		model.addAttribute("message", "Hello Kosa3333!!!");
 		
 		return "hello";
 	}
